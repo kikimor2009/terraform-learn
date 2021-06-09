b80b86719a303ca83497e9baf59854ab014461d2
Artur Zbykovskyi <artur_zbykovskyi@epam.com>
2021-06-09 13:20:12 +0300

    create custom modules

--- aws-project/main.tf
+++ aws-project/main.tf
@@ -1,18 +1,8 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA533VD6GOVJPUDYE3"
-    secret_key = "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"
+    access_key = "AKIA4WCN3RXZPH4OKYDC"
+    secret_key = "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"
 }
-
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-
-
 
 resource "aws_vpc" "lab-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -21,104 +11,21 @@ resource "aws_vpc" "lab-vpc" {
     }
 }
 
-resource "aws_subnet" "lab-subnet-1" {
+module "lab-subnet" {
+    source = "./modules/subnet"
     vpc_id = aws_vpc.lab-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name = "${var.env_prefix}-subnet-1"
-    }
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
 }
 
-resource "aws_route_table" "lab-route-table" {
+module "webserver-instance" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.lab-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.lab-igw.id
-    }
-    tags = {
-        Name = "${var.env_prefix}-rtbl"
-    }
-}
-
-resource "aws_internet_gateway" "lab-igw" {
-    vpc_id = aws_vpc.lab-vpc.id
-    tags = {
-        Name = "${var.env_prefix}-igw"
-    }
-}
-
-resource "aws_route_table_association" "aws-rtb-subnet" {
-    subnet_id = aws_subnet.lab-subnet-1.id
-    route_table_id = aws_route_table.lab-route-table.id
-}
-
-resource "aws_security_group" "lab-sg" {
-    vpc_id = aws_vpc.lab-vpc.id
-    name = "lab-sg"
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.my_ip]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-
-    tags = {
-        Name = "${var.env_prefix}-sg"
-    }
-}
-
-data "aws_ami" "latest-aws-linux" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "lab-key"
-    public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "lab-nginx" {
-    ami = data.aws_ami.latest-aws-linux.id
+    my_ip = var.my_ip
+    subnet_id = module.lab-subnet.subnet.id 
+    env_prefix = var.env_prefix
+    public_key_location = var.public_key_location
     instance_type = var.instance_type
-
-    subnet_id = aws_subnet.lab-subnet-1.id
-    vpc_security_group_ids = [aws_security_group.lab-sg.id]
-    availability_zone = var.avail_zone
-
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    /*user_data = <<EOF
-                    #!/bin/bash
-                    sudo yum update -y
-                EOF
-    */
-
-    #user_data = file("entry-script.sh")
-
-    tags = {
-        Name = "${var.env_prefix}-server"
-    }
+    avail_zone = var.avail_zone
 }
---
+++ aws-project/modules/subnet/outputs.tf
@@ -0,0 +1,3 @@
+output subnet {
+  value       = aws_subnet.lab-subnet-1
+}
